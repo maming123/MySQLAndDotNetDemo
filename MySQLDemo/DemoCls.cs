e887e5ede6c74da3f7dc0ed59075e53fb060ed4b
maming123 <hurricanefbi@163.com>
2015-11-20 19:56:52 +0800

    MySQL 数据库和Asp.net  解决Petapoco T4  针对MySql char（36）生成Guid错误的问题

---
+++ MySQLDemo/DemoCls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Models.RCS;
+
+namespace MySQLDemo
+{
+    public class DemoCls
+    {
+        public static void GetUserRole()
+        {
+            sys_role_permisson sysRP = new sys_role_permisson() { ID = Guid.NewGuid(), IsDelete = true, PermissonID = Guid.NewGuid(), PermissonType = 1, RoleID = Guid.NewGuid(), Version = 1 };
+            sysRP.Insert();
+            sys_user sysUser = sys_user.SingleOrDefault((object)"3712AB63-CB72-4028-9021-1E39737B4944");
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using MySQLDemo;
+using Util;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DemoCls.GetUserRole();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ExecuteDataTableTest();
+        }
+        private static void ExecuteDataTableTest()
+        {
+
+            string sql = "select * from city";
+            DataTable dt = MySQLHelper.ExecuteDataTable(sql);
+            foreach (DataRow dr in dt.Rows)
+            {
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    Console.Write("{0}:{1}\t", dc.ColumnName, dr[dc.ColumnName].ToString());
+                }
+            }
+        }
+
+        private static void InsertTest()
+        {
+            string sql = "insert into user(Id,Name) values(?Id,?Name);";
+            MySqlParameter[] spArr = new MySqlParameter[]{
+                new MySqlParameter("Id",2),
+                new MySqlParameter("Name","leaf")
+            };
+            int r = MySQLHelper.ExecuteNonQuery(sql, spArr);
+        }
+
+        private static void TestConnection()
+        {
+            //测试连接是否正常
+            using (MySqlConnection conn = MySQLHelper.GetConnection)
+            {
+                try
+                {
+                    conn.Open();
+                    //Console.WriteLine("打开 My SQL 连接成功！");
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("打开 MySQL 连接失败，错误原因：" + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
